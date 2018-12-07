efe1ce7feff06ab1d70cd07bfe26eef945d1839b
ixarchakos <xarchakos@cs.toronto.edu>
2018-12-07 09:32:06 -0500

    testing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import timedelta, datetime
 from flask import Flask, render_template, request, session, redirect, url_for, jsonify, flash
 from flask_socketio import SocketIO,emit, join_room
 from json import loads
-import os
+import os, time
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
 
 app = Flask(__name__)
@@ -29,10 +29,12 @@ socketio = SocketIO(app)
 @app.route("/get_image_prediction", methods=['POST'])
 def get_image_prediction(dataset):
 	region = request.json['region']
-	image_list = os.listdir(ROOT_DIR + '/static/images/' + request.json['dataset'])
+	# image_list = os.listdir(ROOT_DIR + '/static/images/' + request.json['dataset'])
+	image_list = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'll', 'ww']
 	for i in image_list:
 		print(i)
-	return jsonify({"result": resultset})
+		time.sleep(5) 
+	return jsonify({"result": {'value':i}})
 
 @app.route("/index")
 @app.route("/", methods=["GET"])
@@ -41,5 +43,5 @@ def index():
     return render_template("home.html")
 
 if __name__ == '__main__':
-	
+
 	socketio.run(app, port=5008, debug=True)
