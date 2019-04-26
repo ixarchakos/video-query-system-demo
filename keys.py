b47c912cba280be3a26b1a35621d0ae48e3da331
Yannis Xarchakos <xarchakos@cs.toronto.edu>
2019-04-26 03:22:56 -0400

    Delete keys.py

--- keys.py
+++
@@ -1,6 +0,0 @@
-from os import path
-
-APP_ID = "VJcsZwtmpTC0SpTbcsVU1aOiH2OqTvTIoNMpMRST"
-REST_KEY = "X8Q6lCZVTrPFpuw97xjuGENdp43sOIIE0NJ6Mv2y"
-APP_ROOT = path.dirname(path.abspath(__file__))
-IMAGE_FOLDER = "images"
